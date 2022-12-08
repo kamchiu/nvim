1c92009b422d92019095c7def042d17cb9a4d6d3
kamchiu <gumchiulau@gmail.com>
2022-12-08 14:58:29 +0800

    add leap.nvim

--- init.lua
+++ init.lua
@@ -1,24 +1,25 @@
-require "user.impatient"
-require "user.options"
-require "user.keymaps"
-require "user.which-key"
-require "user.plugins"
-require "user.autocommands"
-require "user.markdown"
-require "user.colorscheme"
-require "user.cmp"
-require "user.telescope"
-require "user.gitsigns"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.illuminate"
-require "user.indentline"
-require "user.alpha"
-require "user.lsp"
-require "user.dap"
+require("user.impatient")
+require("user.options")
+require("user.keymaps")
+require("user.which-key")
+require("user.plugins")
+require("user.autocommands")
+require("user.markdown")
+require("user.colorscheme")
+require("user.cmp")
+require("user.telescope")
+require("user.gitsigns")
+require("user.treesitter")
+require("user.autopairs")
+require("user.comment")
+require("user.nvim-tree")
+require("user.bufferline")
+require("user.lualine")
+require("user.toggleterm")
+require("user.project")
+require("user.illuminate")
+require("user.indentline")
+require("user.alpha")
+require("user.lsp")
+require("user.dap")
+require("user.leap")
---
+++ lua/user/leap.lua
@@ -0,0 +1,11 @@
+require("leap").add_default_mappings()
+
+-- Disable auto jump first match
+
+require("leap").opts.safe_labels = {}
+
+require("leap").opts.highlight_unlabeled_phase_one_targets = true
+
+-- vim.keymap.set({'x', 'o', 'n'}, 'r', '<Plug>(leap-forward-to)')
+
+-- vim.keymap.set({'x', 'o', 'n'}, 'R', '<Plug>(leap-backward-to)')
